e59b50b97491e0f609fa72c3f50087ec86760aa1
Théorisons <34948793+theorisons@users.noreply.github.com>
2017-12-29 22:45:10 +0100

    Rename CODE_Projet_LED___f_luminosit__.ino to codeLedFonctionLuminosite.ino

---
+++ codeLedFonctionLuminosite.ino
@@ -0,0 +1,29 @@
+int pinLed [5] = {4, 6, 8, 10, 12} ; //Déclaration tableau une dimension contenant les pins associés aux LEDs
+unsigned long valeurLuminosite = 0 ;  //Variable qui contiendra la valeur de la luminosité
+
+void setup() {
+
+  for (int i = 0 ; i < 5 ; i++) //Affectation des pins comme étant des sorties + définition de l'état initial des LEDs
+  {
+    pinMode (pinLed[i], OUTPUT) ;
+    digitalWrite (pinLed[i], LOW) ;
+  }
+  Serial.begin (9600) ;
+}
+
+void loop() {
+
+  valeurLuminosite = analogRead (A1) ;  //On lit la valeur de l'intensité (on la stock)
+  valeurLuminosite = map (valeurLuminosite, 0, 1023, 0, 5) ;  //On remplace la valeur de la luminosité par une valeur entre 0 et 5
+  Serial.println (valeurLuminosite) ;
+  for (int i = 0; i < valeurLuminosite; i++) //On allume le nombre de LED adequat
+  {
+    digitalWrite (pinLed [i], HIGH);
+  }
+
+  delay (10) ;
+  for (int i = 0; i < valeurLuminosite; i++) //On les éteins
+  {
+    digitalWrite (pinLed [i], LOW);
+  }
+}
